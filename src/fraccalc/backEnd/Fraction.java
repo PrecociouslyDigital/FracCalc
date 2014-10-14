9047ce496e760244ea46446a799485db919e5743
precociouslyDigital <fellfromatree@gmail.com>
2014-10-14 09:14:20 -0700

    Amended!

---
+++ src/fraccalc/backEnd/Fraction.java
@@ -0,0 +1,7 @@
+package fraccalc.backEnd;
+
+public class Fraction {
+	public Fraction(String washington){
+		
+	}
+}
---
+++ src/fraccalc/backEnd/OperatedFraction.java
@@ -0,0 +1,10 @@
+package fraccalc.backEnd;
+
+public class OperatedFraction {
+	public Fraction fraction;
+	public Operator operator;
+	public OperatedFraction(Fraction frank, Operator opera){
+		this.fraction = frank;
+		this.operator = opera;
+	}
+}
