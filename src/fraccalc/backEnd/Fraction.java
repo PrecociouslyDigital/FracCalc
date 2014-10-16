3a503e96181da9017a5ea615c114833183950fcd
Benjamin <fellfromatree@gmail.com>
2014-10-15 17:28:15 -0700

    Wrote the constructor.
    I wrote the constructor. In the web browser, cause friggin netbeans is just not opening this project for some reason.

--- src/fraccalc/backEnd/Fraction.java
+++ src/fraccalc/backEnd/Fraction.java
@@ -1,7 +1,31 @@
 package fraccalc.backEnd;
 
 public class Fraction {
+	public int whole;
+	public int numerator;
+	public int denominator;
 	public Fraction(String washington){
+		if(washington.indexOf("/") != -1){
+			boolean hasWhole = false;
+			String york;
+			String wyoming;
+			if(washington.indexOf("_") != -1){
+				this.whole = washington.split("_", 1)[0];
+				hasWhole = true;
+			}else{
+				this.whole = 0;
+			}
+			york = washington.split("/", 1)[0];
+			if(whole)
+				york = york.split("_", 1)[1];
+			wyoming = washington.split("/", 1)[1];
+			numerator = Integer.parseInt(york);
+			denominator = Integer.parseInt(wyoming);
+		}else{
+			this.whole = Integer.parseInt(washington);
+			this.numerator = 0;
+			this.denominator = 0;
+		}
 		
 	}
 }
